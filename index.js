2da1bed25849d7c9cbb6e05e1e88807f061522ff
BharsadiaParth <parth@iide.co>
2024-10-05 12:00:52 +0530

    dockerfile changes

--- index.js
+++ index.js
@@ -2,14 +2,15 @@
 
 const hapi = require("@hapi/hapi");
 const {userRoutes} = require("./routes/user-routes");
+const env = require("dotenv").config({path: "./.env"});
 
 
 const init = async () => {
     const server = hapi.server({
-        port: 8082,
-        host: 'localhost'
+        port: process.env.PORT,
+        host: '0.0.0.0'
     });
-
+    console.log(process.env.PORT)
     server.start()
     .then((val) => {
         console.log("hapi is listening on port:", server.info.port)
